6f452a5a7ef10d7382a9196f355ae9e43eef3e03
Alan Cabrera <holasoyalan9@gmail.com>
2023-06-30 02:11:41 -0600

    Almos done??, works but not works xd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+function useClicks(initialItem){
+  let [item, setItem] = React.useState(initialItem);
+
+  const newClick = () => {
+    setItem(item++);
+  }
+
+  return [item, newClick];
+}
+
 function App() {
+
+  const [number, setNumber] = useClicks(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <p>{number}</p>
+      </div>
+      <div>
+        <button onClick={setNumber}>Reset</button>
+        <button onClick={setNumber}>Click</button>
+      </div>
+    </>
   );
 }
 
